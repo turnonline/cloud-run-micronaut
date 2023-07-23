08f8e2823d1d08681aeaeb7834b3086914654a75
Aurel Medvegy <medvegy@turnonline.biz>
2023-07-23 12:39:31 +0200

    firebase-admin dropped (missing fraalvm support). micronaut version upgrade (4.0.1)

--- src/main/java/biz/turnonline/ecosystem/skeleton/CloudRunner.java
+++ src/main/java/biz/turnonline/ecosystem/skeleton/CloudRunner.java
@@ -3,11 +3,6 @@ package biz.turnonline.ecosystem.skeleton;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.ServiceOptions;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseAuth;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
@@ -19,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 
@@ -30,32 +24,14 @@ public class CloudRunner
 {
     private static final Logger LOGGER = LoggerFactory.getLogger( CloudRunner.class );
 
-    private final FirebaseAuth auth;
-
     private final ObjectMapper mapper;
 
-    public CloudRunner() throws IOException
+    CloudRunner()
     {
-        this( initFirebase() );
-    }
-
-    CloudRunner( FirebaseAuth auth )
-    {
-        this.auth = auth;
-
         this.mapper = new ObjectMapper();
         this.mapper.setDateFormat( new SimpleDateFormat( DATE_FORMAT_PATTERN ) );
         this.mapper.registerModule( new JavaTimeModule() );
         this.mapper.configure( DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false );
-    }
-
-    private static FirebaseAuth initFirebase() throws IOException
-    {
-        FirebaseApp.initializeApp( FirebaseOptions.builder()
-                .setProjectId( ServiceOptions.getDefaultProjectId() )
-                .setCredentials( GoogleCredentials.getApplicationDefault() )
-                .build() );
-        return FirebaseAuth.getInstance();
     }
 
     @Get( produces = MediaType.TEXT_PLAIN )
